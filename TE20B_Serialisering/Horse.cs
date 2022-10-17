31532ceaef24ea94f8b9be20b050dabb1c107792
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-10-17 13:03:28 +0200

    Initial commit

---
+++ TE20B_Serialisering/Horse.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Horse
+{
+  public string Color { get; set; }
+  public int Speed { get; set; }
+  public float Scale { get; set; }
+}
---
+++ TE20B_Serialisering/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Text.Json;
+
+string whinnyText = File.ReadAllText("whinny.txt");
+
+
+Horse whinny = JsonSerializer.Deserialize<Horse>(whinnyText);
+
+Console.WriteLine(whinny.Speed);
+
+// Horse whinny = new Horse();
+
+// whinny.Color = "Rainbow";
+// whinny.Scale = 200;
+// whinny.Speed = 3;
+
+// string whinnyText = JsonSerializer.Serialize<Horse>(whinny);
+
+// File.WriteAllText("whinny.txt", whinnyText);
+
+
+Console.ReadLine();
